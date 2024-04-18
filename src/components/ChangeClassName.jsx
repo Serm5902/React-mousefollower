56297c76aa61622d664dfb5819c884002733df01
serm5902 <serm5902@gmail.com>
2024-04-17 21:33:01 -0500

    add comments

--- src/components/ChangeClassName.jsx
+++ src/components/ChangeClassName.jsx
@@ -6,11 +6,18 @@ export function ChangeClassName({ enabled }) {
   useEffect(() => {
     document.body.classList.toggle('no-cursor', enabled);
     const btn = document.body.getElementsByTagName('button');
+    // La anterior linea nos devuelve una lista con todos los elements
+    // de tipo button.
+    // Luego esa lista la convertimos en un array, al cual le pasamos
+    // el método de forEach para que evalué cada item del array
+    // y luego a cada element de la lista le pase la clase
+    // no-cursor solo si enabled es verdadero.
     Array.from(btn).forEach((b) => b.classList.toggle('no-cursor', enabled));
 
     return () => {
       // cleanup method
       document.body.classList.remove('no-cursor');
+      // Limpiamos e array de btn de la clase no-cursor
       Array.from(btn).forEach((b) => b.classList.remove('no-cursor'));
     };
   }, [enabled]);
